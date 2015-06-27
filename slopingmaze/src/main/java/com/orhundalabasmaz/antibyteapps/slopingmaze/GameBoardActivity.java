bd76a42b5b03acaec00ffed1a9c702e40f9138f7
odalabasmaz <odalabasmaz@gmail.com>
2015-06-27 16:58:04 +0300

    horizontal move capacity added

--- slopingmaze/src/main/java/com/orhundalabasmaz/antibyteapps/slopingmaze/GameBoardActivity.java
+++ slopingmaze/src/main/java/com/orhundalabasmaz/antibyteapps/slopingmaze/GameBoardActivity.java
@@ -8,13 +8,24 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 
 import com.orhundalabasmaz.antibyteapps.middleware.base.BaseActivity;
 
 
-public class GameBoardActivity extends BaseActivity {
+public class GameBoardActivity
+		extends BaseActivity
+		implements SensorEventListener {
+
 	private Sensor mSensor;
 	private SensorManager mSensorManager;
+	private boolean mInitialized;
+	private float mLastX, mLastY, mLastZ;
+//	private final float NOISE = (float) 2.0;
+	private final float THRESHOLD = (float) 0.01;
+	private final float SENSIBILITY = (float) 20;
+	private ImageView image;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,33 +36,89 @@ public class GameBoardActivity extends BaseActivity {
 
 	private void initSensor() {
 		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-		mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-//		mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-
-		mSensorManager.registerListener(new SensorEventListener() {
-			@Override
-			public void onSensorChanged(SensorEvent event) {
-				if (event.sensor == mSensor) {
-					findTextViewById(R.id.infoText).setText("Info: " + event.values[0]);
-
-				}
-
-				//todo if reaches the edges
-				if (false) {
-					mSensorManager.unregisterListener(this);
-				}
-			}
-
-			@Override
-			public void onAccuracyChanged(Sensor sensor, int accuracy) {
-			}
-		}, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+		mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
+		mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
+	}
 
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mSensorManager.unregisterListener(this);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+
+		float x = event.values[0];
+		float y = event.values[1];
+		float z = event.values[2];
+
+		if (!mInitialized) {
+			mLastX = x;
+			mLastY = y;
+			mLastZ = z;
+			image = (ImageView) findViewById(R.id.angrybird);
+			mInitialized = true;
+		} else {
+			float deltaX = Math.abs(mLastX - x);
+			float deltaY = Math.abs(mLastY - y);
+			float deltaZ = Math.abs(mLastZ - z);
+//			if (deltaX < NOISE) deltaX = (float) 0.0;
+//			if (deltaY < NOISE) deltaY = (float) 0.0;
+//			if (deltaZ < NOISE) deltaZ = (float) 0.0;
+
+			/*if (deltaX > deltaY) {
+				findTextViewById(R.id.infoText).setText("horizontal");
+			} else if (deltaY > deltaX) {
+				findTextViewById(R.id.infoText).setText("vertical");
+			}*/
+
+			final float leftPos = image.getX();
+//			final int rightPos = image.getRight();
+//			final int horizontalPos = leftPos;
+
+			float diff = y - mLastY;
+			if (Math.abs(diff) < THRESHOLD) diff = 0;
+
+			if (diff > 0 || mLastY > 0) {
+				findTextViewById(R.id.infoText).setText("right");
+				image.setX(leftPos + SENSIBILITY);
+			} else if (diff < 0 || mLastY < 0) {
+				findTextViewById(R.id.infoText).setText("left");
+				image.setX(leftPos - SENSIBILITY);
+			} else {
+				// do nothing
+			}
+			findTextViewById(R.id.anglesText).setText("X: " + mLastX + ", Y: " + mLastY + " Z: " + mLastZ);
+
+			mLastX = x;
+			mLastY = y;
+			mLastZ = z;
+		}
+
+		//todo if reaches the edges
+		if (isReachTheEdges()) {
+			mSensorManager.unregisterListener(this);
+		}
+	}
+
+	private boolean isReachTheEdges() {
+		return false;
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 	}
 
 	@Override
